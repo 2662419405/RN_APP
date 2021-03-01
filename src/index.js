314f7691c37471b8f137608c695025de68c0543a
孙航 <2662419405@qq.com>
2021-03-01 22:58:51 +0800

    更新状态栏

--- src/index.js
+++ src/index.js
@@ -1,14 +1,37 @@
 import React, {PureComponent} from 'react';
-import {Text} from 'react-native';
+import {View, StyleSheet, Platform, StatusBar, StatusBar} from 'react-native';
+import {WebView} from 'react-native-webview';
+
+const STATUS_HEIGHT =
+  Platform.OS === 'ios'
+    ? 20
+    : Platform.Version > 19
+    ? StatusBar.currentHeight
+    : 0;
 
 class App extends PureComponent {
   render() {
     return (
-      <>
-        <Text>123</Text>
-      </>
+      <View style={[styles.container, styles.status]}>
+        <StatusBar translucent={true} backgroundColor="transparent" />
+        <WebView
+          originWhitelist={['*']}
+          source={{
+            uri: 'https://h5.shtodream.cn',
+          }}
+          style={[styles.container]}></WebView>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  status: {
+    paddingTop: STATUS_HEIGHT,
+  },
+});
 
 export default App;
